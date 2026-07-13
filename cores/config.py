514974c15ba40da80d5c4a974618e468ca3ff5d4
hu <hu@hudeMac-mini.local>
2026-07-13 09:12:50 +0800

    环境配置

---
+++ cores/config.py
@@ -0,0 +1,27 @@
+# **********************************************************
+#                   配置
+# **********************************************************
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from tools.constants import HOME_DIR
+from functools import lru_cache
+
+env_file = HOME_DIR / ".env"  # env文件
+
+
+class AgentSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=env_file,
+        env_file_encoding="utf-8",
+        extra="ignore",
+        case_sensitive=True,
+    )
+
+    # ====== llm ====== #
+    OPENAI_MODEL: str
+    OPENAI_API_KEY: str
+    OPENAI_URL: str
+
+
+@lru_cache()
+def get_settings():
+    return AgentSettings()
---
+++ tools/constants.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+HOME_DIR = Path(__file__).resolve().parent.parent
